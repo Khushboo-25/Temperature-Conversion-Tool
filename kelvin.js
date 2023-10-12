981caa5392a976fe49830ab9e2e13d3eb7d948d1
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2023-10-12 22:41:00 +0530

    Update kelvin.js

--- kelvin.js
+++ kelvin.js
@@ -1,60 +1,59 @@
-function kelvinConverter()
+function solve1()
 {
-    let kvl=document.getElementById('kelvin');
-    let frn=document.getElementById('forenhieght');
-    let cel=document.getElementById('celsius');
-    let kl=kvl.value;
-    if (!isNaN(kl)) {
-        let cl=(kl-273);
-        let fn=(cl * 9 / 5) +32;
+    let kelvin=document.getElementById('kelvin');
+    let forenhieght=document.getElementById('forenhieght');
+    let celsius=document.getElementById('celsius');
+    let kvl=kelvin.value;
+    if (!isNaN(kvl)) {
+        let cel=(kvl-273);
+        let frn=(cel * 9 / 5) +32;
 
-        cel.textContent = cl.toFixed(2) + " °C";
-        frn.textContent = fn.toFixed(2) + " °F";
+        celsius.textContent = cel.toFixed(2) + " °C";
+        forenhieght.textContent = frn.toFixed(2) + " °F";
     }
     else
     {
-        cel.textContent =  "Invalid Input";
-        frn.textContent = "Invalid Input";
+        celsius.textContent =  "Invalid Input";
+        forenhieght.textContent = "Invalid Input";
     }
     
 }
-function celsiusConverter()
+function solve2()
 {
-    let kvl2=document.getElementById('kelvin2');
-    let frn2=document.getElementById('forenhieght2');
-    let cel2=document.getElementById('celsius2');
-    let cl2=parseFloat(cel2.value);
-    if (!isNaN(cl2)) {
-        let kl2=(cl2+273);
-        let fn2=(cl2 * (9 / 5))+32 ;
+    let kelvin2=document.getElementById('kelvin2');
+    let forenhieght2=document.getElementById('forenhieght2');
+    let celsius2=document.getElementById('celsius2');
+    let cel2=parseFloat(celsius2.value);
+    if (!isNaN(cel2)) {
+        let kvl2=(cel2+273);
+        let frn2=(cel2 * (9 / 5))+32 ;
 
-        frn2.textContent = fn2.toFixed(2) + " °F";
-        kvl2.textContent = kl2.toFixed(2) + " K";
+        forenhieght2.textContent = frn2.toFixed(2) + " °F";
+        kelvin2.textContent = kvl2.toFixed(2) + " K";
     }
     else
     {
-        kvl2.textContent =  "Invalid Input";
-        frn2.textContent = "Invalid Input";
+        kelvin2.textContent =  "Invalid Input";
+        forenhieght2.textContent = "Invalid Input";
     }
     
 }
-function forenhieghtConverter()
+function solve3()
 {
-    let kvl3=document.getElementById('kelvin3');
-    let frn3=document.getElementById('forenhieght3');
-    let cel3=document.getElementById('celsius3');
-    let fn3=parseFloat(frn3.value);
-    if (!isNaN(fn3)) {
-        let cl3=((fn3-32)*(5/9));
-        let kl3=((fn3-32) * (5 / 9)) +273;
+    let kelvin3=document.getElementById('kelvin3');
+    let forenhieght3=document.getElementById('forenhieght3');
+    let celsius3=document.getElementById('celsius3');
+    let frn3=parseFloat(forenhieght3.value);
+    if (!isNaN(frn3)) {
+        let cel3=((frn3-32)*(5/9));
+        let kvl3=((frn3-32) * (5 / 9)) +273;
 
-        cel3.textContent = cl3.toFixed(2) + " °C";
-        kvl3.textContent = kl3.toFixed(2) + " K";
+        celsius3.textContent = cel3.toFixed(2) + " °C";
+        kelvin3.textContent = kvl3.toFixed(2) + " K";
     }
     else
     {
-        cel3.textContent =  "Invalid Input";
-        kvl3.textContent= "Invalid Input";
+        celsius3.textContent =  "Invalid Input";
+        kelvin3.textContent= "Invalid Input";
     }
-    
 }
